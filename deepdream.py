1136ae67e43b17ceac073dc515de149233171ba8
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2018-01-03 11:53:22 +0100

    add multiscale tile vis

--- deepdream.py
+++ deepdream.py
@@ -3,6 +3,7 @@ import mxnet as mx
 
 from collections import namedtuple, OrderedDict
 
+import numpy as np
 import tqdm as tqdm
 from PIL import Image
 from mxnet.visualization import print_summary
@@ -23,7 +24,7 @@ def rework_symbol_file(prefix, layername, channel):
         symbol_json = json.load(json_file)
     layers = get_all_layernames(symbol_json['nodes'])
 
-    # strip all unecessary parts of network and add new loss layer
+    # strip all unecessary parts of network and add new loss layers
     head_id = layers[layername]
     symbol_json['nodes'] = symbol_json['nodes'][:head_id + 1]
 
@@ -77,26 +78,74 @@ def array_to_image(array):
     return image
 
 
-rework_symbol_file('caffenet', 'conv5', 2)
-symbol, arg_params, aux_params = mx.model.load_checkpoint('caffenet', 0000)
+def naive_feature_vis(mod, data, image):
+    for _ in tqdm.tqdm(range(40)):
+        mod.forward(Batch([data]))
+        mod.backward()
 
-print_summary(symbol, shape={'data': (1, 3, 224, 224)})
+        input_gradients = mod.get_input_grads()[0].asnumpy()
+        input_gradients /= input_gradients.std() + 1e-8
 
-mod = mx.mod.Module(symbol, context=mx.gpu(0))
-mod.bind(data_shapes=[('data', (1, 3, 224, 224))], inputs_need_grad=True)
-mod.set_params(arg_params=arg_params, aux_params=aux_params, allow_missing=True, allow_extra=True)
+        data += mx.nd.array(input_gradients, ctx=mx.gpu(0))
+        image = array_to_image(data.asnumpy()[0])
+    return image
 
-data = mx.nd.random.uniform(shape=(1, 3, 224, 224), ctx=mx.gpu(0))
 
-image = array_to_image(data.asnumpy()[0])
-for i in tqdm.tqdm(range(40)):
-    mod.forward(Batch([data]))
-    mod.backward()
+def calc_grad_tiled(mod, data, tile_size=224):
+    image_height, image_width = data.shape[-2:]
+    shift_x, shift_y = np.random.randint(tile_size, size=2)
 
-    input_gradients = mod.get_input_grads()[0].asnumpy()
-    input_gradients /= input_gradients.std() + 1e-8
+    shifted_image = np.roll(np.roll(data.asnumpy(), shift_x, 3), shift_y, 2)
+    grad = mx.nd.zeros_like(data, ctx=mx.gpu(0))
 
-    data += mx.nd.array(input_gradients, ctx=mx.gpu(0))
+    for y in range(0, max(image_height - tile_size // 2, tile_size), tile_size):
+        for x in range(0, max(image_width - tile_size // 2, tile_size), tile_size):
+            tiled_crop = mx.nd.array(shifted_image[:, :, y:y+tile_size, x:x+tile_size], ctx=mx.gpu(0))
+            mod.forward_backward(Batch([tiled_crop]))
+            gradients = mod.get_input_grads()[0]
+            grad[:, :, y:y+tile_size, x:x+tile_size] = gradients
+    return np.roll(np.roll(grad.asnumpy(), -shift_x, 3), -shift_y, 2)
+
+
+def render_multiscale(mod, data, image):
+    num_octaves = 5
+    num_steps = 10
+    octave_scale = 1.5
+    zoom_matrix = mx.nd.array([[1, 0, 0], [0, 1, 0]], ctx=mx.gpu(0))
+    zoom_matrix = mx.nd.reshape(zoom_matrix, shape=(1, 6))
+
+    for octave in tqdm.tqdm(range(num_octaves)):
+        if octave > 0:
+            new_height = int(image.height * octave_scale)
+            new_width = int(image.width * octave_scale)
+            image = image.resize((new_width, new_height), Image.BILINEAR)
+
+            grid = mx.nd.GridGenerator(data=zoom_matrix, transform_type='affine', target_shape=(new_height, new_width))
+            data = mx.nd.BilinearSampler(data, grid)
+
+        for i in range(num_steps):
+            g = calc_grad_tiled(mod, data)
+            g /= g.std() + 1e-8
+            data += mx.nd.array(g, ctx=mx.gpu(0))
+            image = array_to_image(data.asnumpy()[0])
+    return image
+
+
+if __name__ == "__main__":
+    # adjust symbol file
+    rework_symbol_file('caffenet', 'conv4', 100)
+
+    # build model
+    symbol, arg_params, aux_params = mx.model.load_checkpoint('caffenet', 0000)
+    print_summary(symbol, shape={'data': (1, 3, 224, 224)})
+
+    mod = mx.mod.Module(symbol, context=mx.gpu(0))
+    mod.bind(data_shapes=[('data', (1, 3, 224, 224))], inputs_need_grad=True)
+    mod.set_params(arg_params=arg_params, aux_params=aux_params, allow_missing=True, allow_extra=True)
+
+    # prepare input data
+    data = mx.nd.random.uniform(shape=(1, 3, 224, 224), ctx=mx.gpu(0))
+
     image = array_to_image(data.asnumpy()[0])
-
-image.show()
+    image = render_multiscale(mod, data, image)
+    image.show()
